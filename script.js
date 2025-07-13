643498e06b8e66ff51745bf1cd0c6ff078454930
vpoupin <vpouping@gmail.com>
2025-07-13 10:40:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const ramos = [
+  // 1er Semestre
+  { id: "anatomia", nombre: "Anatomía", requisitos: [] },
+  { id: "histologia", nombre: "Histología", requisitos: [] },
+  { id: "biocel", nombre: "Biología Celular y Molecular", requisitos: [] },
+  { id: "quimica_general", nombre: "Química General", requisitos: [] },
+  { id: "matematicas", nombre: "Matemáticas", requisitos: [] },
+  { id: "alimentos1", nombre: "Alimentos 1", requisitos: [] },
+  { id: "intro_prof", nombre: "Introducción a la Profesión", requisitos: [] },
+  // ... resto omitido por brevedad en esta celda
+];
